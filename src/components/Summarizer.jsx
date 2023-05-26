0b9c1ac0c660af68be88d554fdf81c319111e3ec
kamalafidele <kamalafizzet45@gmail.com>
2023-05-26 11:49:16 +0300

    added the script in request body

--- src/components/Summarizer.jsx
+++ src/components/Summarizer.jsx
@@ -8,7 +8,7 @@ import Loader from './loader';
 
 
 function Summarizer() {
-  const [text, setText] = useState('');
+  const [script, setScript] = useState('');
   const [summary, setSummary] = useState('');
   const [isLoading, setIsLoading] = useState(false);
 
@@ -16,9 +16,9 @@ function Summarizer() {
     e.preventDefault();
     setIsLoading(true);
 
-    axios.post(`${API_URL}/summarize`, { text })
+    axios.post(`${API_URL}/summarize`, { script })
      .then(({ data }) => setSummary(data.summary))
-     .catch((e) => setSummary('ChatGPT failed to summarize the text'))
+     .catch((e) => setSummary('An issue occurred with ChatGPT integration'))
      .finally(() => setIsLoading(false));
   }
 
@@ -32,7 +32,7 @@ function Summarizer() {
       <div className='content'>
         <div className='row'>
         <label htmlFor='script' style={{ display: 'block', marginBottom: 10 }}>TV Script</label>
-        <textarea rows={19} cols={80} placeholder='Enter the script' onChange={(e) => setText(e.target.value)}></textarea>
+        <textarea rows={19} cols={80} placeholder='Enter the script' onChange={(e) => setScript(e.target.value)}></textarea>
         </div>
         <div className="row">
           <button onClick={(e) => handleSubmit(e)}>Summarize</button>
